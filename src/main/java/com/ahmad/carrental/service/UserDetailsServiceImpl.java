f9ae1f45388d946a730128e8b692a69b7d3c8672
ahmadm99 <ahmad.m.mansour99@gmail.com>
2021-08-08 14:44:05 +0300

    Autowired bcrypt correctly

--- src/main/java/com/ahmad/carrental/service/UserDetailsServiceImpl.java
+++ src/main/java/com/ahmad/carrental/service/UserDetailsServiceImpl.java
@@ -4,11 +4,14 @@ import com.ahmad.carrental.model.Role;
 import com.ahmad.carrental.model.User;
 import com.ahmad.carrental.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
